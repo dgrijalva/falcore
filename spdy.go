6b3f6c8fe17b2e1200295cdc3409018d2e92aa48
Dave Grijalva <grijalva@gmail.com>
2012-10-07 14:36:16 -0700

    spdy connection needs a struct.  lots of state to track

--- spdy.go
+++ spdy.go
@@ -2,48 +2,88 @@ package falcore
 
 import (
 	"net"
-	"code.google.com/p/go.net/spdy"
 	"time"
+
+	"code.google.com/p/go.net/spdy"
 )
+
+type spdyConnection struct {
+	conn net.Conn
+	framer *spdy.Framer
+	streams map[uint32]chan spdy.Frame
+	sendChan chan spdy.Frame
+	closeChan chan int
+	goaway bool
+}
 
 func (srv *Server) spdyHandler(c net.Conn){
 	// Create a framer
-	var framer *spdy.Framer
 	var err error
-
-	var closeSentinelChan = make(chan int)
-	go srv.spdySentinel(c, closeSentinelChan)
-	defer srv.connectionFinished(c, closeSentinelChan)
+	var session = new(spdyConnection)
 
 	// FIXME: should we be using buffered reader/writers?
-	if framer, err = spdy.NewFramer(c, c); err != nil {
+	if session.framer, err = spdy.NewFramer(c, c); err != nil {
 		Error("Couldn't get spdy.Framer for conn: %v", err)
 		srv.connectionFinished(c, nil)
 		return
 	}
+
+	session.conn = c
+	session.streams = make(map[uint32]chan spdy.Frame)
+	session.sendChan = make(chan spdy.Frame)
+	session.closeChan = make(chan int)
+	session.goaway = false
 	
-	var frame spdy.Frame
-	keepalive := true
-	for err == nil && keepalive {
-		if frame, err = framer.ReadFrame(); err == nil {
-			switch frame.(type) {
-			case *spdy.GoAwayFrame:
-				keepalive = false
-			}
+	go srv.spdyWriter(session)
+	defer srv.connectionFinished(c, session.closeChan)
+	
+	srv.spdyReader(session)
+}
+
+func (srv *Server) spdyHandleStream(f *spdyConnection, fchan chan spdy.Frame) {
+	for {
+		_, ok := <- fchan
+		if ok {
+			
+		} else {
+			
 		}
 	}
-	
 }
 
-func (srv *Server) spdyHandleStream(f *spdy.Framer, frame spdy.Frame) {
-	
+func (srv *Server) spdyReader(session *spdyConnection) {
+	var frame spdy.Frame
+	var err error
+	keepalive := true
+	for err == nil && keepalive {
+		if frame, err = session.framer.ReadFrame(); err == nil {
+			switch fr := frame.(type) {
+			case *spdy.NoopFrame:
+				// Do nothing
+			case *spdy.GoAwayFrame:
+				session.goaway = true
+			case *spdy.PingFrame:
+				session.sendChan <- frame
+			case *spdy.SynStreamFrame:
+				if !session.goaway {
+					c := make(chan spdy.Frame)
+					session.streams[fr.StreamId] = c
+					go srv.spdyHandleStream(session, c)
+					c <- frame
+				}
+			}
+		}
+		if session.goaway && len(session.streams) == 0 {
+			keepalive = false
+		}
+	}
 }
 
-func (srv *Server) spdySentinel(c net.Conn, connClosed chan int) {
+func (srv *Server) spdyWriter(session *spdyConnection) {
 	select {
 	case <-srv.stopAccepting:
 		// TODO: send goaway frame, etc
-		c.SetReadDeadline(time.Now().Add(3 * time.Second))
-	case <-connClosed:
+		session.conn.SetReadDeadline(time.Now().Add(3 * time.Second))
+	// case <-connClosed:
 	}
 }
